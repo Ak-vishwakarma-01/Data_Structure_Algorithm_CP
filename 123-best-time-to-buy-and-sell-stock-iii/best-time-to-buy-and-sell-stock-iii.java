e88446c571d3bc231928b94bbdba30f2b93446c3
Ankit kumar Vishwakarma <100838272+Ak-vishwakarma-01@users.noreply.github.com>
2025-03-20 15:25:59 +0530

    Time: 62 ms (34.57%) | Memory: 64.4 MB (29.94%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,27 +1,23 @@
 class Solution {
     int dp[][][];
-    int count(int ind, int prices[], int purchased, int times, int n){
-        if(times==0)return 0;
-        if(ind>=n)return 0;
-        if(dp[ind][purchased][times-1] != -1) return dp[ind][purchased][times-1];
-        int localTransaction = Integer.MIN_VALUE;
-        if(purchased==1){ // purchased
-            int sell = prices[ind] + count(ind + 1, prices, 0,times-1, n);
-            int notSell = count(ind +1, prices, 1,times, n);
-            localTransaction = Math.max(sell, notSell);
-        }else{ // not purchased
-            int buy = - prices[ind] + count(ind +1,prices, 1,times, n);
-            int notBuy = count(ind +1, prices , 0,times, n);
-            localTransaction = Math.max(localTransaction,Math.max(buy, notBuy));
-        }
-        return dp[ind][purchased][times-1]=localTransaction;
-    }
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        dp = new int[n+1][2][2];
-        for(int i[][]: dp){
-            for(int de[]:i) Arrays.fill(de,-1);
+        dp = new int[n + 1][2][3];
+        for (int i[][] : dp) {
+            for (int de[] : i)
+                Arrays.fill(de, 0);
         }
-        return count(0, prices, 0,2, n);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int purchased = 0; purchased <= 1; purchased++) {
+                for (int transactions = 1; transactions <= 2; transactions++) {
+                    if (purchased == 1) { // purchased
+                        dp[i][purchased][transactions] = Math.max(prices[i] + dp[i+1][0][transactions-1] , dp[i+1][1][transactions]);
+                    } else { // not purchased
+                        dp[i][purchased][transactions] = Math.max( -prices[i] + dp[i+1][1][transactions], dp[i+1][0][transactions]);
+                    }
+                }
+            }
+        }
+        return dp[0][0][2];
     }
 }
