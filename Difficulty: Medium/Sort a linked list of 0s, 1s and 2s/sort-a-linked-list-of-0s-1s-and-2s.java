54a2678d6283c9794eca1d3532062d04b2079cb3
Ankit kumar Vishwakarma <100838272+Ak-vishwakarma-01@users.noreply.github.com>
2025-09-06 00:03:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.java
@@ -1,101 +1,42 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
+/*
 class Node {
     int data;
     Node next;
 
-    Node(int x) {
-        data = x;
-        next = null;
-    }
-}
-
-
-// } Driver Code Ends
-
-/* class Node
-{
-    int data;
-    Node next;
-    Node(int data)
+    Node(int d)
     {
-        this.data = data;
+        data = d;
         next = null;
     }
 }*/
+
 class Solution {
-    static Node segregate(Node head) {
-        if (head == null || head.next == null) return head;
-
-        Node zeroHead = new Node(0), oneHead = new Node(0), twoHead = new Node(0);
-        Node zero = zeroHead, one = oneHead, two = twoHead;
-
-        Node curr = head;
-
-        while (curr != null) {
-            if (curr.data == 0) {
-                zero.next = curr;
-                zero = zero.next;
-            } else if (curr.data == 1) {
-                one.next = curr;
-                one = one.next;
-            } else {
-                two.next = curr;
-                two = two.next;
-            }
-            curr = curr.next;
+    public Node segregate(Node head) {
+        // code here
+        
+        List<Integer> z = new LinkedList<>();
+        List<Integer> o = new LinkedList<>();
+        List<Integer> t = new LinkedList<>();
+        Node dede = head;
+        while(head!=null){
+            if(head.data==0) z.add(0);
+            else if(head.data==1) o.add(1);
+            else t.add(2);
+            head = head.next;
         }
-
-        // Connect the three lists
-        zero.next = (oneHead.next != null) ? oneHead.next : twoHead.next;
-        one.next = twoHead.next;
-        two.next = null; // last node should point to null
-
-        return zeroHead.next;
+        head = dede;
+        for(int i: z){
+            head.data = i;
+            head = head.next;
+        }
+        for(int i: o){
+            head.data = i;
+            head = head.next;
+        }
+        for(int i: t){
+            head.data = i;
+            head = head.next;
+        }
+        return dede;
     }
 }
-
-
-
-
-//{ Driver Code Starts.
-
-class GFG {
-    public static void printList(Node node) {
-        while (node != null) {
-            System.out.print(node.data + " ");
-            node = node.next;
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-
-        while (t-- > 0) {
-            List<Integer> arr = new ArrayList<>();
-            String input = br.readLine().trim();
-            StringTokenizer st = new StringTokenizer(input);
-            while (st.hasMoreTokens()) {
-                arr.add(Integer.parseInt(st.nextToken()));
-            }
-
-            Node head = null;
-            if (!arr.isEmpty()) {
-                head = new Node(arr.get(0));
-                Node tail = head;
-                for (int i = 1; i < arr.size(); i++) {
-                    tail.next = new Node(arr.get(i));
-                    tail = tail.next;
-                }
-            }
-            head = new Solution().segregate(head);
-            printList(head);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
